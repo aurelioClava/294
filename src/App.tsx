302d13594c6333142f80eea820fbc0da96f56a89
Aurelio Clavadetscher <clavadetscher@vfei.ch>
2024-01-22 16:23:25 +0100

    tag 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import  DemoPage from './Kurs/page'
+
+function App() {
+  return (
+    <>
+    <h1>Hello</h1>
+    <div>
+      <DemoPage></DemoPage>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Kurs/page.tsx
@@ -0,0 +1,10 @@
+import { DataTable } from "./data-table"
+
+export default  function DemoPage() {
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable/>
+    </div>
+  )
+}
